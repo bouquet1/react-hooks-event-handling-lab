ca5efc64c8bb38a0d16ccc9eb7f87579d1639dd8
bouquet1 <bukuleta@gmail.com>
2023-06-02 20:23:47 -0700

    Keypad.js and EyesOnMe.js components done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  return (
+    <div>
+      <input type="password" id="password" onChange={(e) => console.log("Entering password...")} />
+    </div>
+  );
 }
 
 export default Keypad;
+
+// <label htmlFor="username">Username</label>
+// <input type="text" id="username" />
